68c06c975395f9c5d5d1a42b90bae55d581469a9
hyecheon <rainbow880616@gmail.com>
2019-11-24 21:29:29 +0900

    init

---
+++ backend/src/main/java/com/hyecheon/socialexample/SocialExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hyecheon.socialexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialExampleApplication.class, args);
+    }
+
+}
