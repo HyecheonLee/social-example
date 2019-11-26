37ff3783fafe70b0627e50da216231c315f5d71a
hyecheon <rainbow880616@gmail.com>
2019-11-27 01:38:55 +0900

    user Signup front

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -1,26 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
 
-function UserSignupPage() {
+function UserSignupPage({actions}) {
+    let [state, setState] = useState({
+        displayName: "",
+        username: "",
+        password: "",
+        passwordRepeat: "",
+        pendingApiCall: false
+    });
+
+    const onChange = (e) => {
+        const {name, value} = e.target;
+        setState({
+            ...state,
+            [name]: value
+        })
+    };
+    const onClickSignup = (e) => {
+        if (actions) {
+            const user = {
+                username: state.username,
+                displayName: state.displayName,
+                password: state.password,
+            };
+            setState({...state, pendingApiCall: true});
+            actions.postSignup(user)
+                .then(response => {
+                    setState({...state, pendingApiCall: false});
+                })
+                .catch(error => {
+                    setState({...state, pendingApiCall: false});
+                });
+        }
+    };
     return (
-        <div>
-            <h1>가입</h1>
-            <div>
-                <input placeholder={"당신의 아이디"}/>
+        <div className="container">
+            <h1 className="text-center">가입</h1>
+            <div className="col-12 mb-3">
+                <label>아이디</label>
+                <input
+                    className="form-control"
+                    name={"displayName"} value={state.displayName} onChange={onChange}
+                    placeholder={"당신의 아이디"}/>
             </div>
-            <div>
-                <input placeholder={"당신의 성함"}/>
+            <div className="col-12 mb-3">
+                <label>이름</label>
+                <input className="form-control" name={"username"} value={state.username} onChange={onChange}
+                       placeholder={"당신의 성함"}/>
             </div>
-            <div>
-                <input type="password" placeholder={"당신의 비밀번호"}/>
+            <div className="col-12 mb-3">
+                <label>비밀번호</label>
+                <input className="form-control" name={"password"} value={state.password} type="password"
+                       onChange={onChange}
+                       placeholder={"당신의 비밀번호"}/>
             </div>
-            <div>
-                <input type="password" placeholder={"비밀번호 확인"}/>
+            <div className="col-12 mb-3">
+                <label>비밀번호 확인</label>
+                <input className="form-control" name={"passwordRepeat"} value={state.passwordRepeat} type="password"
+                       onChange={onChange}
+                       placeholder={"비밀번호 확인"}/>
             </div>
-            <div>
-                <button>가입</button>
+            <div className="text-center">
+                <button disabled={state.pendingApiCall} className="btn btn-primary" onClick={onClickSignup}>
+                    {state.pendingApiCall && (
+                        <div className="spinner-border text-light spinner-border-sm mr-sm-1" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>)}
+                    가입
+                </button>
             </div>
         </div>
     );
 }
 
+UserSignupPage.defaultProps = {
+    actions: {
+        postSignup: () => new Promise((resolve, reject) => {
+            resolve({});
+        })
+    }
+};
 export default UserSignupPage;
--- frontend/src/pages/UserSignupPage.spec.js
+++ frontend/src/pages/UserSignupPage.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {render, cleanup} from "@testing-library/react";
+import {render, cleanup, fireEvent, waitForDomChange} from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import UserSignupPage from "./UserSignupPage";
 
@@ -48,4 +48,161 @@ describe("UserSignupPage", () => {
             expect(button).toBeInTheDocument();
         });
     });
+    describe("Inter actions", () => {
+        const mockAsyncDelayed = () => jest.fn().mockImplementation(() => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve({});
+                }, 300)
+            })
+        });
+        const changeEvent = (value) => {
+            return {
+                target: {
+                    value
+                }
+            }
+        };
+        let button, displayNameInput, usernameInput, passwordInput, passwordRepeatInput;
+        const setupForSubmit = (props) => {
+            let rendered = render(<UserSignupPage {...props}/>);
+            const {container, queryByPlaceholderText} = rendered;
+            displayNameInput = queryByPlaceholderText('당신의 아이디');
+            usernameInput = queryByPlaceholderText('당신의 성함');
+            passwordInput = queryByPlaceholderText('당신의 비밀번호');
+            passwordRepeatInput = queryByPlaceholderText('비밀번호 확인');
+            button = container.querySelector('button');
+
+            fireEvent.change(displayNameInput, changeEvent("displayName"));
+            fireEvent.change(usernameInput, changeEvent("username"));
+            fireEvent.change(passwordInput, changeEvent("password"));
+            fireEvent.change(passwordRepeatInput, changeEvent("passwordRepeat"));
+
+            return rendered;
+        };
+
+        it("sets the displayName value into state", () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage/>);
+            const displayNameInput = queryByPlaceholderText('당신의 아이디');
+            const changeEvent = {
+                target: {
+                    value: 'my-display-name'
+                }
+            };
+            fireEvent.change(displayNameInput, changeEvent);
+            expect(displayNameInput).toHaveValue('my-display-name');
+        });
+        it("sets the username value into state", () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage/>);
+            const usernameInput = queryByPlaceholderText('당신의 성함');
+            const changeEvent = {
+                target: {
+                    value: 'username'
+                }
+            };
+            fireEvent.change(usernameInput, changeEvent);
+            expect(usernameInput).toHaveValue('username');
+        });
+        it("sets the password value into state", () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage/>);
+            const passwordInput = queryByPlaceholderText('당신의 비밀번호');
+            const changeEvent = {
+                target: {
+                    value: 'password'
+                }
+            };
+            fireEvent.change(passwordInput, changeEvent);
+            expect(passwordInput).toHaveValue('password');
+        });
+        it("sets the passwordRepeat value into state", () => {
+            const {queryByPlaceholderText} = render(<UserSignupPage/>);
+            const passwordRepeatInput = queryByPlaceholderText('비밀번호 확인');
+            const changeEvent = {
+                target: {
+                    value: 'passwordRepeat'
+                }
+            };
+            fireEvent.change(passwordRepeatInput, changeEvent);
+            expect(passwordRepeatInput).toHaveValue('passwordRepeat');
+        });
+        it("calls postSignup when the fields are valid and the actions are provided in props", () => {
+            const actions = {
+                postSignup: jest.fn().mockResolvedValueOnce({})
+            };
+            setupForSubmit({actions});
+            fireEvent.click(button);
+            expect(actions.postSignup).toHaveBeenCalledTimes(1)
+        });
+        it("does not throw exception when clicking the button when actions not provided in props", () => {
+            setupForSubmit();
+            expect(() => fireEvent.click(button).not.toThrow())
+        });
+        it("calls post with user body when the fields are valid", () => {
+            const actions = {
+                postSignup: jest.fn().mockResolvedValueOnce({})
+            };
+            setupForSubmit({actions});
+            fireEvent.click(button);
+            const expectedUserObject = {
+                username: "username",
+                displayName: "displayName",
+                password: "password",
+            };
+            expect(actions.postSignup).toHaveBeenCalledWith(expectedUserObject);
+        });
+        xit("does not allow user to click the sign up button when there is an ongoing api call", () => {
+            const actions = {
+                postSignup: mockAsyncDelayed()
+            };
+            setupForSubmit({actions});
+            fireEvent.click(button);
+
+            fireEvent.click(button);
+            expect(actions.postSignup).toHaveBeenCalledTimes(1);
+        });
+        xit("display spinner when there is an orgoing api call", () => {
+            const actions = {
+                postSignup: mockAsyncDelayed()
+            };
+            const {queryByText} = setupForSubmit({actions});
+            fireEvent.click(button);
+
+            const spinner = queryByText("Loading...");
+            expect(spinner).toBeInTheDocument();
+        });
+        it("hides spinner after api call finishes successfully", async () => {
+            const actions = {
+                postSignup: mockAsyncDelayed()
+            };
+            const {queryByText} = setupForSubmit({actions});
+            fireEvent.click(button);
+
+            await waitForDomChange();
+
+            const spinner = queryByText("Loading...");
+            expect(spinner).not.toBeInTheDocument();
+        });
+        it("hides spinner after api call finishes error", async () => {
+            const actions = {
+                postSignup: jest.fn().mockImplementation(() => {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            reject({
+                                response:{data:{}}
+                            });
+                        }, 300)
+                    })
+                })
+            };
+            const {queryByText} = setupForSubmit({actions});
+            fireEvent.click(button);
+
+            await waitForDomChange();
+
+            const spinner = queryByText("Loading...");
+            expect(spinner).not.toBeInTheDocument();
+        });
+    })
 });
+console.error = () => {
+};
