d53aa8c9526e864ac0769bd28f592ca777098460
hyecheon <rainbow880616@gmail.com>
2019-12-06 00:10:50 +0900

    Handling Input Change

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../components/Input";
+
 export default function LoginPage() {
+  const [state, setState] = useState({ username: "" });
+  const onChange = event => {
+    const { value, name } = event.target;
+    setState({
+      ...state,
+      [name]: value
+    });
+  };
   return (
     <div className="container">
       <h1 className="text-center">Login</h1>
       <div className="col-12 mb-3">
-        <Input label="Username" placeholder="Your username" />
+        <Input
+          label="Username"
+          placeholder="Your username"
+          value={state.username}
+          onChange={onChange}
+          name="username"
+        />
       </div>
       <div className="col-12 mb-3">
-        <Input type="password" label="Password" placeholder="Your password" />
+        <Input
+          name="password"
+          onChange={onChange}
+          type="password"
+          label="Password"
+          placeholder="Your password"
+        />
       </div>
       <div className="text-center">
         <button className="btn btn-primary">Login</button>
