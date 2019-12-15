c7192c9c0f2bc604c5806e14d950f00153e326c7
hyecheon <rainbow880616@gmail.com>
2019-12-15 19:26:21 +0900

    Combining Signup And Login Actions

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,38 +1,33 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import UserSignupPage from "../pages/UserSignupPage";
 import UserPage from "../pages/UserPage";
-import * as apiCalls from "../api/apiCalls";
 import TopBar from "../components/TopBar";
 
-const actions = {
-  postLogin: apiCalls.login,
-  postSignup: apiCalls.signup
-};
 
 function App() {
-  return (
-    <div>
-      <TopBar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route
-            exact
-            path="/login"
-            component={props => <LoginPage {...props} actions={actions} />}
-          />
-          <Route
-            path="/signup"
-            component={props => <UserSignupPage {...props} actions={actions} />}
-          />
-          <Route path="/:username" component={UserPage} />
-        </Switch>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<TopBar/>
+			<div className="container">
+				<Switch>
+					<Route exact path="/" component={HomePage}/>
+					<Route
+						exact
+						path="/login"
+						component={props => <LoginPage {...props} />}
+					/>
+					<Route
+						path="/signup"
+						component={props => <UserSignupPage {...props}/>}
+					/>
+					<Route path="/:username" component={UserPage}/>
+				</Switch>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- frontend/src/redux/auth.js
+++ frontend/src/redux/auth.js
@@ -1,4 +1,4 @@
-import * as apiCalls from '../api/apiCalls';
+import * as apiCalls from "../api/apiCalls";
 
 const LOGOUT_SUCCESS = "auth/LOGOUT_SUCCESS";
 const LOGIN_SUCCESS = "auth/LOGIN_SUCCESS";
@@ -15,18 +15,25 @@ export const logout = () => ({
 	type: LOGOUT_SUCCESS,
 	auth: {...initialState}
 });
-export const loginSuccess = (response) => ({
+export const loginSuccess = response => ({
 	type: LOGIN_SUCCESS,
 	auth: {...response, isLoggedIn: true}
 });
 
-export const loginHandler = (credentials) => {
+export const loginHandler = credentials => {
 	return function (dispatch) {
 		const {password} = credentials;
 		return apiCalls.login(credentials).then(response => {
-			dispatch(
-				loginSuccess({...response.data, password})
-			);
+			dispatch(loginSuccess({...response.data, password}));
+			return response;
+		});
+	};
+};
+export const signupHandler = user => {
+	return function (dispatch) {
+		return apiCalls.signup(user).then(response => {
+			dispatch(loginHandler(user));
+			return response;
 		});
 	};
 };
