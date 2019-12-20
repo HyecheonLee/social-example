a31e1b841bf9a98ab136c4e0de35343edffee04f
hyecheon <rainbow880616@gmail.com>
2019-12-20 23:42:34 +0900

    User ListApi

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
+export function listUsers(param = {page: 0, size: 3}) {
+  return axios.get(
+      `/api/1.0/users?page=${param.page || 0}&size=${param.size || 3}`);
+}
+
 export const signup = user => {
   return axios.post("/api/1.0/users", user);
 };
 export const login = user => {
-  return axios.post("/api/1.0/login", {}, { auth: user });
+  return axios.post("/api/1.0/login", {}, {auth: user});
 };
-export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
+export const setAuthorizationHeader = ({username, password, isLoggedIn}) => {
   if (isLoggedIn) {
     axios.defaults.headers.common["Authorization"] = `Basic ${btoa(
-      username + ":" + password
+        username + ":" + password
     )}`;
   } else {
     delete axios.defaults.headers.common["Authorization"];
