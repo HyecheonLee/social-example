ae9c6d1821ad8f92afc9dee1c11d8f61e61a450e
hyecheon <rainbow880616@gmail.com>
2019-12-22 22:54:46 +0900

    User Update Api

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -1,19 +1,27 @@
 import axios from "axios";
 
+const USER_URL = "/api/1.0/users";
+const LOGIN_URL = "/api/1.0/login";
+
+export function updateUser(userId, body) {
+  return axios.put(`${USER_URL}/${userId}`, body);
+}
+
 export function getUser(username) {
-  return axios.get(`/api/1.0/users/${username}`);
+  return axios.get(`${USER_URL}/${username}`);
 }
 
 export function listUsers(param = {page: 0, size: 3}) {
   return axios.get(
-      `/api/1.0/users?page=${param.page || 0}&size=${param.size || 3}`);
+      `${USER_URL}?page=${param.page || 0}&size=${param.size || 3}`);
 }
 
 export const signup = user => {
-  return axios.post("/api/1.0/users", user);
+  return axios.post(USER_URL, user);
 };
 export const login = user => {
-  return axios.post("/api/1.0/login", {}, {auth: user});
+
+  return axios.post(LOGIN_URL, {}, {auth: user});
 };
 export const setAuthorizationHeader = ({username, password, isLoggedIn}) => {
   if (isLoggedIn) {
