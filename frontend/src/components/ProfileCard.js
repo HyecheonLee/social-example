09e59be35b5e871d1f9da93e2b791be934050830
hyecheon <rainbow880616@gmail.com>
2019-12-23 00:01:11 +0900

    User Edit Layout

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,25 +1,48 @@
-import React from 'react';
-import defaultPicture from '../assets/profile.png';
-import {Link} from "react-router-dom";
+import React from "react";
+import defaultPicture from "../assets/profile.png";
+import { Link } from "react-router-dom";
 import ProfileImageWithDefault from "./ProfileImageWithDefault";
+import Input from "./Input";
 
-function ProfileCard({user, isEditable}) {
+function ProfileCard({ user, isEditable, inEditMode }) {
   return (
-      <div className="card">
-        <div className="card-header text-center">
-          <ProfileImageWithDefault alt={"profile"} width={200} height={200}
-                                   src={user.imag}
-                                   className="rounded-circle shadow"
-          />
-        </div>
-        <div className="card-body text-center">
-          <h4>{`${user.displayName}@${user.username}`}</h4>
-          {isEditable && <button
-              className="btn btn-outline-success">
-            <i className="fas fa-user-edit"/> Edit
-          </button>}
-        </div>
+    <div className="card">
+      <div className="card-header text-center">
+        <ProfileImageWithDefault
+          alt={"profile"}
+          width={200}
+          height={200}
+          src={user.imag}
+          className="rounded-circle shadow"
+        />
       </div>
+      <div className="card-body text-center">
+        {!inEditMode && <h4>{`${user.displayName}@${user.username}`}</h4>}
+        {inEditMode && (
+          <div className="mb-2">
+            <Input
+              value={user.displayName}
+              label={`Change Display Name for ${user.username}`}
+            />
+          </div>
+        )}
+        {isEditable && !inEditMode && (
+          <button className="btn btn-outline-success">
+            <i className="fas fa-user-edit" /> Edit
+          </button>
+        )}
+        {isEditable && inEditMode && (
+          <>
+            <button className="btn btn-primary">
+              <i className="fas fa-save" /> Save
+            </button>
+            <button className="btn btn-secondary ml-1">
+              <i className="fas fa-window-close" /> Cancel
+            </button>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
 
