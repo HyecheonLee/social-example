0a10bc51dde1b64c56bc6f60cb433e2c7d5400fe
hyecheon <rainbow880616@gmail.com>
2019-12-23 20:28:10 +0900

    Static Resource Checks

--- backend/src/test/java/com/hyecheon/socialexample/StaticResourceTest.java
+++ backend/src/test/java/com/hyecheon/socialexample/StaticResourceTest.java
@@ -1,23 +1,36 @@
 package com.hyecheon.socialexample;
 
 import com.hyecheon.socialexample.configuration.AppConfiguration;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.engine.support.descriptor.ClasspathResourceSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.File;
+import java.io.IOException;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class StaticResourceTest {
     @Autowired
     private AppConfiguration appConfiguration;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void checkStaticFolder_whenAppIsInitialized_uploadFolderMustExist() {
@@ -41,5 +54,41 @@ public class StaticResourceTest {
         final File attachmentFolder = new File(attachmentsFolderPath);
         final boolean attachmentFolderFolderExists = attachmentFolder.exists() && attachmentFolder.isDirectory();
         assertThat(attachmentFolderFolderExists).isTrue();
+    }
+
+    @Test
+    void getStaticFile_whenImageExistInProfileUploadFolder_receiveOk() throws Exception {
+        String fileName = "profile-picture.png";
+        final File source = new ClassPathResource("profile.png").getFile();
+
+        final File target = new File(appConfiguration.getFullProfileImagePath() + "/" + fileName);
+        FileUtils.copyFile(source, target);
+
+        mockMvc.perform(get("/images/" + appConfiguration.getProfileImageFolder() + "/" + fileName)).andExpect(status().isOk());
+
+    }
+
+    @Test
+    void getStaticFile_whenImageExistInAttachmentFolder_receiveOk() throws Exception {
+        String fileName = "profile-picture.png";
+        final File source = new ClassPathResource("profile.png").getFile();
+
+        final File target = new File(appConfiguration.getFullAttachmentsPath() + "/" + fileName);
+        FileUtils.copyFile(source, target);
+
+        mockMvc.perform(get("/images/" + appConfiguration.getAttachmentsFolderPath() + "/" + fileName)).andExpect(status().isOk());
+
+    }
+
+    @Test
+    void getStaticFile_whenImageDoesNotExist_receiveNotFound() throws Exception {
+        mockMvc.perform(get("/images/" + appConfiguration.getAttachmentsFolderPath() + "/there-is-no-such-image.png"))
+                .andExpect(status().isNotFound());
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullProfileImagePath()));
+        FileUtils.cleanDirectory(new File(appConfiguration.getFullAttachmentsPath()));
     }
 }
